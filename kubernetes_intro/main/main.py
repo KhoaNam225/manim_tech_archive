5265cb3e4aaa4e64868eac0b8d8687420e30e965
Nam Pham <pknam25@gmail.com>
2025-01-28 22:18:44 +0800

    Added a first template for manim with some absic animation

---
+++ kubernetes_intro/main/main.py
@@ -0,0 +1,17 @@
+from manim import Scene, Circle, Square, Create, Transform, FadeOut
+from manim import PINK, RIGHT, TAU
+
+
+class DefaultTemplate(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.flip(RIGHT)  # flip horizontally
+        square.rotate(-3 * TAU / 8)  # rotate a certain amount
+        square.scale(0.5)
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
