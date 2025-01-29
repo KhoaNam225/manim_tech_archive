c74caa91b50d8763722b2fa3d7564a8a004ec859
Nam Pham <pknam25@gmail.com>
2025-01-29 16:29:14 +0800

    Added some logos along with some animation for it

--- kubernetes_intro/main/main.py
+++ kubernetes_intro/main/main.py
@@ -1,4 +1,13 @@
-from manim import Scene, Circle, Square, Create, Transform, FadeOut
+from manim import (
+    GrowFromCenter,
+    Scene,
+    Circle,
+    Square,
+    Create,
+    Transform,
+    FadeOut,
+    SVGMobject,
+)
 from manim import PINK, RIGHT, TAU
 
 
@@ -12,6 +21,12 @@ class DefaultTemplate(Scene):
         square.rotate(-3 * TAU / 8)  # rotate a certain amount
         square.scale(0.5)
 
+        docker_logo = SVGMobject(file_name="../assets/docker-logo.svg")
+        server_icon = SVGMobject(file_name="../assets/server-icon.svg")
+
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+        self.play(GrowFromCenter(docker_logo))
+        self.play(FadeOut(docker_logo))
+        self.play(GrowFromCenter(server_icon))
